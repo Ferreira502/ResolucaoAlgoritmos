2d268648c751bbe8ec9de52834f3b65208205b9b
Ferreira <gabrielferreirape502@gmail.com>
2026-01-08 18:05:41 -0300

    adding 1548

---
+++ BeeCrowd/Nivel_4/1548/Main.java
@@ -0,0 +1,51 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt(); 
+
+        for (int l = 0; l < n; l++) 
+        {
+            int m = sc.nextInt(); 
+
+            int[] original = new int[m];
+            int[] ordenado = new int[m];
+
+            for (int i = 0; i < m; i++) 
+            {
+                original[i] = sc.nextInt();
+                ordenado[i] = original[i];
+            }
+
+            for (int i = 0; i < m - 1; i++) 
+            {
+                for (int j = 0; j < m - 1 - i; j++) 
+                {
+                    if (ordenado[j] < ordenado[j + 1]) 
+                    {
+                        int aux = ordenado[j];
+                        ordenado[j] = ordenado[j + 1];
+                        ordenado[j + 1] = aux;
+                    }
+                }
+            }
+
+            int cont = 0;
+
+            for (int i = 0; i < m; i++) 
+            {
+                if (original[i] == ordenado[i]) 
+                {
+                    cont++;
+                }
+            }
+
+            System.out.println(cont);
+        }
+
+        sc.close();
+    }
+}
--- BeeCrowd/Nivel_7/2872/main.c
+++ BeeCrowd/Nivel_7/2872/main.c
@@ -1,54 +1,54 @@
-// #include<stdio.h>
-// #include<string.h>
+#include<stdio.h>
+#include<string.h>
 
-// #define MAX 20
+#define MAX 20
 
-// void swap(int *i, int *j) 
-// {
-//    int temp = *i;
-//    *i = *j;
-//    *j = temp;
-// }
+void swap(int *i, int *j) 
+{
+   int temp = *i;
+   *i = *j;
+   *j = temp;
+}
 
-// void bolha(int *array, int n)
-// {
-//     int i = 0, j = 0;
-//     for (i = 0; i < n-1; i++) 
-//     {
-//       for (j = 0; j < n-1; j++) 
-//       {
-//         if (array[j] > array[j + 1]) 
-//         {
-//            swap(&array[j], &array[j + 1]);
-//         }
-//       }
-//    }
-// }
+void bolha(int *array, int n)
+{
+    int i = 0, j = 0;
+    for (i = 0; i < n-1; i++) 
+    {
+      for (j = 0; j < n-1; j++) 
+      {
+        if (array[j] > array[j + 1]) 
+        {
+           swap(&array[j], &array[j + 1]);
+        }
+      }
+   }
+}
 
-// int str_to_int ( char value [ ], int size )
-// {
-//     int sum = 0;
+int str_to_int ( char value [ ], int size )
+{
+    int sum = 0;
 
-//     for ( int i = 0; i < size; i ++ )
-//     {
-//         if ( value [ i ] >= 48 && value [ i ] <= 57 )
-//         {
-//             sum = sum * 10 + ( value [ i ] - '0' );
-//         }
-//     }
+    for ( int i = 0; i < size; i ++ )
+    {
+        if ( value [ i ] >= 48 && value [ i ] <= 57 )
+        {
+            sum = sum * 10 + ( value [ i ] - '0' );
+        }
+    }
 
-//     // printf ( "soma : %d\n", sum );
+    // printf ( "soma : %d\n", sum );
 
-//     return sum;
-// }
+    return sum;
+}
 
-// int main ()
-// {
-//     int opcao = 1;
-//     // while (scanf("%d", &opcao) != EOF)
-//     // {
-//     //     /* code */
-//     // }
+int main ()
+{
+    int opcao = 1;
+    // while (scanf("%d", &opcao) != EOF)
+    // {
+    //     /* code */
+    // }
 
-//     char palavra[MAX];
-// }
+    char palavra[MAX];
+}
