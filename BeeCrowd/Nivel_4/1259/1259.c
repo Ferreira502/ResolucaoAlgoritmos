dfbc5d5d9c95d423ce4699961b77f673c39327d8
Ferreira <gabrielferreirape502@gmail.com>
2026-01-20 19:31:33 -0300

    1259 finished

--- BeeCrowd/Nivel_4/1259/1259.c
+++ BeeCrowd/Nivel_4/1259/1259.c
@@ -1,42 +1,75 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int ordenar (int x, int n)
+void ordenarCrec(int array[], int n)
 {
-    for (int i = 0; i < n; i++)
+    int aux;
+    for (int i = 0; i < n - 1; i++)
     {
-        for (int j = 0; j < n + 1; j++)
+        for (int j = 0; j < n - 1 - i; j++)
         {
-            /* code */
+            if (array[j] > array[j + 1])
+            {
+                aux = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = aux;
+            }
         }
-        
     }
-    
 }
 
-int main () 
+void ordenarDec(int array[], int n)
 {
-    int n = 0;
+    int aux;
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j < n - 1 - i; j++)
+        {
+            if (array[j] < array[j + 1])
+            {
+                aux = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = aux;
+            }
+        }
+    }
+}
+
+int main()
+{
+    int n;
     scanf("%d", &n);
 
     int valor[n];
-    int ordenadopar[n];
-    int ordenadoimpar[n];
+    int par[n], impar[n];
+    int qtdPar = 0, qtdImpar = 0;
 
     for (int i = 0; i < n; i++)
     {
         scanf("%d", &valor[i]);
-        
+
         if (valor[i] % 2 == 0)
         {
-            int ordenadopar = ordernar(valor[i], n);
+            par[qtdPar++] = valor[i];
         }
 
-        else if (valor[i] % 2 != 0)
+        else
         {
-            int ordenadoimpar = ordenar(valor[i], n);
+            impar[qtdImpar++] = valor[i];
         }
-        
-        
     }
-    
+
+    ordenarCrec(par, qtdPar);
+    ordenarDec(impar, qtdImpar);
+
+    for (int i = 0; i < qtdPar; i++)
+    {
+        printf("\n%d", par[i]);
+    }
+
+    for (int i = 0; i < qtdImpar; i++)
+    {
+        printf("\n%d", impar[i]);
+    }
+
+    return 0;
 }
