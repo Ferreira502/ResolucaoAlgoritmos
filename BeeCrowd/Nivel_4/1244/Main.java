30f7892805b79eedc21a934f797efa51522feebe
Ferreira <gabrielferreirape502@gmail.com>
2026-01-29 16:10:28 -0300

    adding 1244

--- BeeCrowd/Nivel_4/1244/Main.java
+++ BeeCrowd/Nivel_4/1244/Main.java
@@ -1,20 +1,42 @@
 import java.util.*;
 
-class Main
+public class Main 
 {
-    public static void main (String[] args)
+    public static void main(String[] args) 
     {
         Scanner sc = new Scanner(System.in);
 
-        int opcao = 0;
-        String[] palavra;
+        int n = sc.nextInt();
+        sc.nextLine();
 
-        
-        while (opcao != 0) 
-        { 
-            palavra = sc.next();
+        while (n > 0) 
+        {
+            String linha = sc.nextLine();
+            String[] p = linha.split("\\s+");
 
-            
+            for (int i = 0; i < p.length - 1; i++) 
+            {
+                for (int j = i + 1; j < p.length; j++) 
+                {
+                    if (p[i].length() < p[j].length()) 
+                    {
+                        String tmp = p[i];
+                        p[i] = p[j];
+                        p[j] = tmp;
+                    }
+                }
+            }
+
+            for (int i = 0; i < p.length; i++) 
+            {
+                if (i > 0) System.out.print(" ");
+                System.out.print(p[i]);
+            }
+            System.out.println();
+
+            n--;
         }
+
+        sc.close();
     }
 }
