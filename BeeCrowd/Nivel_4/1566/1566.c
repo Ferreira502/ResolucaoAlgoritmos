2495685231cd1c6a7ca48291d5b6bc374bf00471
Ferreira <gabrielferreirape502@gmail.com>
2026-02-03 16:01:52 -0300

    solution 1566

---
+++ BeeCrowd/Nivel_4/1566/1566.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+void ordenar(int n, int array[])
+{
+    int aux;
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j < n - 1 - i; j++)
+        {
+            if (array[j] > array[j + 1])
+            {
+                aux = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = aux;
+            }
+        }
+    }
+}
+
+int main()
+{
+    int n = 0;
+    int h = 0;
+
+    scanf("%d", &n);
+
+    while (n != 0)
+    {
+
+        scanf("%d", &h);
+        int pessoas[h];
+
+        for (int i = 0; i < h; i++)
+        {
+            scanf("%d", &pessoas[i]);
+        }
+
+        ordenar(h, pessoas);
+
+        for (int i = 0; i < h; i++)
+        {
+            printf("\n%d", pessoas[i]);
+        }
+
+        n--;
+    }
+}
