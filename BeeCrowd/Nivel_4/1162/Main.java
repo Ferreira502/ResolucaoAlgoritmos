3d78aeac3ed2255ff64d31e6963529da21aa8ffe
Ferreira <gabrielferreirape502@gmail.com>
2026-02-12 15:43:34 -0300

    1162 finished

---
+++ BeeCrowd/Nivel_4/1162/Main.java
@@ -0,0 +1,46 @@
+//RESOLUCAO DA QUESTAO 1162 DO BEECROWD
+
+import java.util.*;
+
+class Main 
+{
+    public static void main (String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int n = 0;
+        int aux = 0;
+        int cont = 0;
+
+        n = sc.nextInt();
+
+        while ( n > 0)
+        {
+            int l = sc.nextInt();
+            int[] array = new int[l]; 
+        
+            for (int i = 0; i < l; i++) 
+            {
+                array[i] = sc.nextInt();
+            }
+
+           for (int i = 1; i < l; i++)
+           {
+               for (int j = 0; j < l - 1; j++)
+               {
+                   if (array[j] > array[j + 1])
+                   {
+                       aux = array[j];
+                       array[j] = array[j + 1];
+                       array[j + 1] = aux;
+                       cont++;
+                   }
+               
+               }
+           }
+            
+            System.out.println("Optimal train swapping takes " + cont + " swaps.");
+            cont = 0;
+            n--;
+        }
+    }
+}
